7195c80f2e09ea6a28fe6a1a7b9bd7cac98154ba
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-12-27 05:46:46 +0000

    Resolve merge conflicts

--- backend/src/main/kotlin/org/skewwhiffy/theory/Pitch.kt
+++ backend/src/main/kotlin/org/skewwhiffy/theory/Pitch.kt
@@ -1,60 +1,19 @@
 package org.skewwhiffy.theory.org.skewwhiffy.theory
 
 data class Pitch(private val note: Note, private val octave: Octave) {
-    private fun addMajorSecond(): Pitch {
-        val newNote = when (note.noteName) {
-            NoteName.C -> Note(NoteName.D, note.accidental)
-            NoteName.D -> Note(NoteName.E, note.accidental)
-            NoteName.E -> Note(NoteName.F, note.accidental.sharp)
-            NoteName.F -> Note(NoteName.G, note.accidental)
-            NoteName.G -> Note(NoteName.A, note.accidental)
-            NoteName.A -> Note(NoteName.B, note.accidental)
-            NoteName.B -> Note(NoteName.C, note.accidental.sharp)
-        }
-        val newOctave = if (note.noteName == NoteName.B) octave.up else octave
-        return Pitch(newNote, newOctave)
-    }
-
     operator fun plus(interval: Interval): Pitch {
-        val defaultResult = when (interval.size) {
-            2 -> addMajorSecond()
-            3 -> this + Interval.major.second + Interval.major.second
-            4 -> this + Interval.major.third + Interval.minor.second
-            5 -> this + Interval.perfect.fourth + Interval.major.second
-            6 -> this + Interval.perfect.fifth + Interval.major.second
-            7 -> this + Interval.major.sixth + Interval.major.second
-            else -> return this.upOctave + Interval(interval.size - 7, interval.offset)
-        }
-        val offset = interval.offset
-        return if (offset <= 0) (1..-offset).fold(defaultResult) { acc, _ -> acc.flat }
-        else (1..offset).fold(defaultResult) { acc, _ -> acc.sharp }
+        TODO()
     }
-
-    val upOctave
-        get() = Pitch(note, octave.up)
-
-    private val flat
-        get() = Pitch(Note(note.noteName, note.accidental.flat), octave)
-
-    private val sharp
-        get() = Pitch(Note(note.noteName, note.accidental.sharp), octave)
-
-    override fun toString() = "$note$octave"
 }
 
 data class Octave(private val octave: Int) {
     companion object {
         val default = Octave(0)
     }
-
-    val up
-        get() = Octave(octave + 1)
-
-    override fun toString() = "Oct($octave)"
 }
 
-data class Note(val noteName: NoteName, val accidental: Accidental) {
-    private constructor(noteName: NoteName) : this(noteName, Accidental(0))
+data class Note(private val noteName: NoteName, private val accidental: Accidental) {
+    constructor(noteName: NoteName) : this(noteName, Accidental(0))
 
     companion object {
         val C = Note(NoteName.C)
@@ -70,14 +29,10 @@ data class Note(val noteName: NoteName, val accidental: Accidental) {
     val aboveMiddleC
         get() = Pitch(this, Octave.default)
 
-    fun octavesAboveMiddleC(octaves: Int) = Pitch(this, Octave(octaves))
-
     val sharp
         get() = Note(noteName, accidental.sharp)
     val flat
         get() = Note(noteName, accidental.flat)
-
-    override fun toString() = "$noteName$accidental"
 }
 
 data class Accidental(private val offset: Int) {
@@ -85,12 +40,6 @@ data class Accidental(private val offset: Int) {
         get() = Accidental(offset + 1)
     val flat
         get() = Accidental(offset - 1)
-
-    override fun toString() = when {
-        offset > 0 -> "x".repeat(offset / 2) + "#".repeat(offset % 2)
-        offset < 0 -> "b".repeat(-offset)
-        else -> ""
-    }
 }
 
 enum class NoteName {
--- backend/src/test/kotlin/org/skewwhiffy/theory/PitchTest.kt
+++ backend/src/test/kotlin/org/skewwhiffy/theory/PitchTest.kt
@@ -14,49 +14,4 @@ class PitchTest {
 
         assertThat(actual).isEqualTo(expected)
     }
-
-    @Test
-    fun canAddMinorInterval() {
-        val expected = Note.G.aboveMiddleC
-
-        val actual = Note.E.aboveMiddleC + Interval.minor.third
-
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun canAddPerfectInterval() {
-        val expected = Note.B.flat.aboveMiddleC
-
-        val actual = Note.F.aboveMiddleC + Interval.perfect.fourth
-
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun canAddAugmentedPerfectInterval() {
-        val expected = Note.E.sharp.aboveMiddleC.upOctave
-
-        val actual = Note.A.aboveMiddleC + Interval.augmented.fifth
-
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun canAddDiminishedNonPerfectInterval() {
-        val expected = Note.C.flat.octavesAboveMiddleC(1)
-
-        val actual = Note.E.aboveMiddleC + Interval.diminished.sixth
-
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun canAddCompoundInterval() {
-        val expected = Note.G.sharp.sharp.octavesAboveMiddleC(2)
-
-        val actual = Note.A.flat.aboveMiddleC + Interval.compound.augmented.seventh.augmented
-
-        assertThat(actual).isEqualTo(expected)
-    }
 }
