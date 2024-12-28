34eb476c487770bace02a19bf376cee62aa5bba3
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-12-28 11:28:44 +0000

    Build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-#cd ./web && yarn build
-#cd ..
+cd ./web && yarn build
+cd ..
 
 backend_source="./backend/src/main/resources/public"
 rm -rf "$backend_source"
 mkdir "$backend_source"
+cp -r ./web/build/* "$backend_source"
