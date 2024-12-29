9d01b03c71ec68a9b08b76e50e15f722676ef2ad
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-12-29 10:08:46 +0000

    Working

--- backend/src/main/kotlin/org/skewwhiffy/theory/controller/FrontendFilter.kt
+++ backend/src/main/kotlin/org/skewwhiffy/theory/controller/FrontendFilter.kt
@@ -2,15 +2,16 @@ package org.skewwhiffy.theory.org.skewwhiffy.theory.controller
 
 import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletException
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
-import java.io.IOException
 
 @Component
-class FrontendFilter : Filter {
+class FrontendFilter(
+    private val resourceLoader: ResourceLoader
+) : Filter {
     override fun doFilter(
         request: ServletRequest,
         response: ServletResponse?,
@@ -18,12 +19,16 @@ class FrontendFilter : Filter {
     ) {
         val req = request as HttpServletRequest
         val requestURI = req.requestURI
-
         if (requestURI.startsWith("/api")) {
             chain.doFilter(request, response)
             return
         }
-
-        request.getRequestDispatcher("/").forward(request, response)
+        val resourcePath = listOf("public", requestURI).joinToString("/") { it.trim('/') }
+        val resource = resourceLoader.getResource("classpath:$resourcePath")
+        if (resource.exists() && resource.isFile) {
+            chain.doFilter(request, response)
+        } else {
+            request.getRequestDispatcher("/").forward(request, response)
+        }
     }
 }
