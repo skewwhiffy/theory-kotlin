f1ab33cf6ad87b585a142a8ffd810088de50777b
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-12-30 12:16:11 +0000

    Tailwind (#7)
    * Top menu
    
    * Tailwind installed
    
    * IDEA change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {BrowserRouter} from "react-router";
 import {Routing} from './scaffold/Routing';
+import {TopMenu} from "./scaffold/TopMenu";
+import './scaffold/tailwind.css';
 
 const App = () =>
   <BrowserRouter>
+    <TopMenu/>
     <Routing/>
   </BrowserRouter>
 
