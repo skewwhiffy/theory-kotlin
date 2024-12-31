185cbc7345ff9191c5d5820c82ab39a2a2bc8c37
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-12-31 07:56:57 +0000

    Attempt to fix backend IJ

--- web/src/scaffold/TopMenu.tsx
+++ web/src/scaffold/TopMenu.tsx
@@ -2,8 +2,8 @@ import {Link} from "react-router";
 import React from "react";
 
 export const TopMenu = () => {
-  return <div className="bg-amber-50 h-24 p-6 flex gap-6">
-    <div>Theory tester</div>
+  return <div className="bg-yellow-50 h-24 p-6 flex gap-6">
+    <div className="text-4xl font-bold font-sans text-blue-500">Theory tester</div>
     <Link to="">home</Link>
     <Link to="test">test</Link>
   </div>
