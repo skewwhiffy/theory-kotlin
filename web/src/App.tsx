c001f041ea8f7841f9ac5ed2c89b3a59eee5e943
Kenny Hung (he) <kennysubs+github@outlook.com>
2025-01-02 11:52:17 +0000

    Version indicator in top bar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,16 @@ import {BrowserRouter} from "react-router";
 import {Routing} from './scaffold/Routing';
 import {TopMenu} from "./scaffold/TopMenu";
 import './scaffold/tailwind.css';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 const App = () =>
-  <BrowserRouter>
-    <TopMenu/>
-    <Routing/>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <TopMenu/>
+      <Routing/>
+    </BrowserRouter>
+  </QueryClientProvider>
 
 export default App;
--- web/src/scaffold/TopMenu.tsx
+++ web/src/scaffold/TopMenu.tsx
@@ -1,19 +1,23 @@
 import {Link} from "react-router";
 import React from "react";
 import {routes} from "./routes";
+import {Version} from "./Version";
 
 const Links = () => {
-  return <div className="flex-col flex justify-end">
-    <div className="flex gap-2">
-      {routes
-        .filter(it => it.menuText)
-        .map(({path, menuText}) => <Link key={path} to={path}>{menuText?.toUpperCase()}</Link>)}
+  return <div className="flex justify-between w-full">
+    <div className="flex-col flex justify-end">
+      <div className="flex gap-2">
+        {routes
+          .filter(it => it.menuText)
+          .map(({path, menuText}) => <Link key={path} to={path}>{menuText?.toUpperCase()}</Link>)}
+      </div>
     </div>
+    <Version/>
   </div>
 }
 export const TopMenu = () => {
   return <div className="bg-yellow-50 h-24 p-6 flex gap-6">
-    <div className="text-4xl font-bold font-sans text-blue-500">Theory tester</div>
+    <div className="text-lg font-bold font-sans text-blue-500">Theory tester</div>
     <Links/>
   </div>
 }
