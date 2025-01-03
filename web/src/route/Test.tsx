7da882d10612b28078a24824e5797e42b76d9214
Kenny Hung (he) <kennysubs+github@outlook.com>
2025-01-03 10:41:48 +0000

    Use abc

--- web/src/route/Test.tsx
+++ web/src/route/Test.tsx
@@ -1,7 +1,12 @@
-import {TestNotation} from "../notation/TestNotation";
+import {TestNotationAbc} from "../notation/TestNotationAbc";
 
 export const Test = () => {
-  return <div className="flex justify-center">
-    <TestNotation/>
+  return <div className="flex-col justify-items-center justify-center">
+    <div className="flex gap-2 w-1/2">
+      <TestNotationAbc/>
+      <p className="w-1/2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata</p>
+    </div>
   </div>
 }
